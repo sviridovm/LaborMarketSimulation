cefe115f5d774fea3e15cab8a66ca9b22fb0febb
sviridov <sviridov@umich.edu>
2025-01-10 22:27:02 -0500

    added my name

--- streamlit.py
+++ streamlit.py
@@ -10,6 +10,17 @@ constants.init()
 st.title('Labor Market Simulator')
 
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write('By: Maksim Sviridov')
+
+with col2:
+    st.page_link('https://github.com/sviridovm', label='GitHub')
+    
+with col3:
+    st.page_link('https://sviridovm.github.io/', label='Website')
+    
+
 pg = st.navigation([st.Page('options.py'), st.Page('simulation.py')])
 pg.run()
-    
